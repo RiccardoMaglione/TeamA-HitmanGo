f89b0e304a08611ec663fd4e6e4048a362c236ab
Grayson1509 <alessiomanna.job@gmail.com>
2020-10-14 18:26:56 +0200

    Throwing Procedure [Complete]
    > Creata procedura di lanci di un'oggetto per il component NODE_ITEM.cs

---
+++ Assets/Core/Scripts/ThrowingSystem/NodeSelectionOperator.cs
@@ -0,0 +1,20 @@
+﻿using HGO.core.data;
+using UnityEngine;
+
+namespace HGO
+{
+    namespace core
+    {
+        public static class NodeSelectionOperator
+        {
+
+            public enum nodeTypeOperation { ALL, ITEM, WALKABLE}
+            public static NodeSelectionOperation CreateNodeSelectionOperator(nodeTypeOperation type, ThrowingData data, Vector3 origin, Node start_node)
+            {
+                return new NodeSelectionOperation(data, origin, type, start_node);
+            }
+
+
+        }
+    }
+}
