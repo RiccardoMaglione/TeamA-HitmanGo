d0db9864b1228c3a3c2ed883f0733ed6264b0c53
leolaz99 <leolaz99@gmail.com>
2020-10-30 14:19:25 +0100

    Added Game UI
    Add the Menu and in Game UI

---
+++ Assets/HGO UI/Scripts/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    [Tooltip("Valore da modificare per decidere la dimensione del bottone quando il mouse è su di esso")]
+    public float dimension;
+
+    public void OnMouseEnter()
+    {
+        transform.localScale += new Vector3(dimension, dimension, dimension); 
+    }
+
+    public void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
---
+++ Assets/HGO UI/Scripts/MenuManager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject startingScreen;
+    public GameObject exitScreen;
+    public GameObject settingScreen;
+    public GameObject stageSelectionScreen;
+    public GameObject creditsScreen;
+    public GameObject gameScreen;
+    public GameObject pauseScreeen;
+
+    public void GoToExitScreen()
+    {
+        exitScreen.SetActive(true);
+        startingScreen.SetActive(false);
+        settingScreen.SetActive(false);
+        stageSelectionScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        gameScreen.SetActive(false);
+        pauseScreeen.SetActive(false);
+    }
+
+    public void GoToStartingScreen()
+    {
+        startingScreen.SetActive(true);
+        exitScreen.SetActive(false);
+        settingScreen.SetActive(false);
+        stageSelectionScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        gameScreen.SetActive(false);
+        pauseScreeen.SetActive(false);
+    }
+
+    public void GoToSettingScreen()
+    {
+        settingScreen.SetActive(true);
+        startingScreen.SetActive(false);
+        exitScreen.SetActive(false);
+        stageSelectionScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        gameScreen.SetActive(false);
+        pauseScreeen.SetActive(false);
+    }
+
+    public void GoToStageSelectionScreen()
+    {
+        stageSelectionScreen.SetActive(true);
+        settingScreen.SetActive(false);
+        startingScreen.SetActive(false);
+        exitScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        gameScreen.SetActive(false);
+        pauseScreeen.SetActive(false);
+    }
+
+    public void GoToCreditsScreen()
+    {
+        stageSelectionScreen.SetActive(false);
+        settingScreen.SetActive(false);
+        startingScreen.SetActive(false);
+        exitScreen.SetActive(false);
+        creditsScreen.SetActive(true);
+        gameScreen.SetActive(false);
+        pauseScreeen.SetActive(false);
+    }
+
+    public void GoToGameScreen()
+    {
+        gameScreen.SetActive(true);
+        exitScreen.SetActive(false);
+        startingScreen.SetActive(false);
+        settingScreen.SetActive(false);
+        stageSelectionScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        pauseScreeen.SetActive(false);      
+    }
+
+    public void GoToPauseScreen()
+    {
+        pauseScreeen.SetActive(true);
+        exitScreen.SetActive(false);
+        startingScreen.SetActive(false);
+        settingScreen.SetActive(false);
+        stageSelectionScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        gameScreen.SetActive(false);     
+    }
+
+    public void ExitApllication()
+    {
+        Application.Quit();
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
