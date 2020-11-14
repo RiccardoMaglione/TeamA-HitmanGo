c9d5e4ee65a765452e96de57920abd114c55413d
leolaz99 <leolaz99@gmail.com>
2020-11-14 17:00:46 +0100

    add ui elements

--- Assets/HGO UI/Scripts/Fullscreen.cs
+++ Assets/HGO UI/Scripts/Fullscreen.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Fullscreen : MonoBehaviour
 {
     static public bool FullS;
+    public Sprite spriteActive;
+    public Sprite spriteNotActive;
+    public Image image;
+
     public void SetFullscreen()
     {
+        if(Screen.fullScreen == true)
+            image.sprite = spriteActive;
+
+        if (Screen.fullScreen == false)
+            image.sprite = spriteNotActive;
+
         Screen.fullScreen = !Screen.fullScreen;
         FullS = Screen.fullScreen;
     }
--- Assets/HGO UI/Scripts/Vsync.cs
+++ Assets/HGO UI/Scripts/Vsync.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Security.Cryptography.X509Certificates;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Vsync : MonoBehaviour
 {
     public int Sync;
     public Text OnOffText;
-    // Start is called before the first frame update
+    public Sprite spriteActive;
+    public Sprite spriteNotActive;
+    public Image image;
+
     void Start()
     {
         Sync = QualitySettings.vSyncCount;
@@ -28,12 +27,14 @@ public class Vsync : MonoBehaviour
     {
         if (Sync == 1)
         {
+            image.sprite = spriteNotActive;
             QualitySettings.vSyncCount = 0;
             Sync = QualitySettings.vSyncCount;
             OnOffText.text = "OFF";
         }
         else if (Sync == 0)
         {
+            image.sprite = spriteActive;
             QualitySettings.vSyncCount = 1;
             Sync = QualitySettings.vSyncCount;
             OnOffText.text = "ON";
