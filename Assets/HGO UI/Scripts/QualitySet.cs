b7dc0ef593cbf5deedb8f98b6f403214dac5a016
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-11-16 19:10:32 +0100

    Add Stamp, DoAnimQuest and QualitySet, add Map Render
    Add script for stamp, script for DoAnimQuest and script for QualitySet, and add image Map Render

---
+++ Assets/HGO UI/Scripts/QualitySet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QualitySet : MonoBehaviour
+{
+    public int GetQuality;
+    public Text TextQuality;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("QualitySet"))
+        {
+            PlayerPrefs.SetInt("QualitySet", 3);
+            TextQuality.text = "High";
+        }
+        else
+        {
+            GetQuality = PlayerPrefs.GetInt("QualitySet");
+            QualitySettings.SetQualityLevel(GetQuality, true);
+            if (GetQuality == 1)
+            {
+                TextQuality.text = "High";
+            }
+            if (GetQuality == 3)
+            {
+                TextQuality.text = "Low";
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ChangeQuality()
+    {
+        if (GetQuality == 1)
+        {
+            QualitySettings.SetQualityLevel(3, true);
+            TextQuality.text = "High";
+            PlayerPrefs.SetInt("QualitySet", 3);
+        }
+        if (GetQuality == 3)
+        {
+            QualitySettings.SetQualityLevel(1, true);
+            TextQuality.text = "Low";
+            PlayerPrefs.SetInt("QualitySet", 1);
+        }
+    }
+}
---
+++ Assets/Scripts/UIScript/DoAnimQuest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoAnimQuest : MonoBehaviour
+{
+    public GameObject[] PointObject;
+    public Vector3[] PointVector;
+    public float SpeedTime;
+
+    private void Start()
+    {
+        for (int i = 0; i < PointObject.Length; i++)
+        {
+            PointVector[i] = PointObject[i].transform.position;
+        }
+        transform.DOPath(PointVector, 30, PathType.CatmullRom, PathMode.Full3D, 10, Color.black).OnWaypointChange(MyCallback);
+        DOTween.timeScale = SpeedTime;
+    }
+
+    void MyCallback(int waypointIndex)
+    {
+        if (waypointIndex >= PointVector.Length - 1)
+        {
+            DOTween.timeScale = 1;
+        }
+    }
+}
